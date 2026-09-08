45e0bf5418af93e34671f9c9f5447e53b3f5e98e
NarasimhaRoyal <pagadalanarasimha080304@email.com>
2026-09-08 14:32:35 +0530

    Day3

---
+++ python notes/Day_03.py
@@ -0,0 +1,25 @@
+# Numeric variables
+a = 10
+b = 20
+
+# Swapping
+a, b = b, a
+
+# String
+name = "Narasimha"
+
+# List
+marks = [80, 93, 90]
+
+# Tuple
+student = ("Narasimha", "CSE", 21)
+
+# Range
+numbers = list(range(1, 6))
+
+print("a =", a)
+print("b =", b)
+print("Name =", name)
+print("Marks =", marks)
+print("Student =", student)
+print("Range =", numbers)
