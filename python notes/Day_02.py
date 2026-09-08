cf2bda88345340d0d872929e16bbb64e6417b3c4
NarasimhaRoyal <pagadalanarasimha080304@email.com>
2026-09-08 14:26:24 +0530

    Day2

---
+++ python notes/Day_02.py
@@ -0,0 +1,21 @@
+# Variable creation
+name = "Narasimha"
+age = 21
+
+# Multiple assignment
+city, country = "Hyderabad", "India"
+
+print(name)
+print(age)
+print(city)
+print(country)
+
+# Reassignment
+age = 22
+
+print("New age:", age)
+
+# Deleting variable
+del country
+
+print(city)
